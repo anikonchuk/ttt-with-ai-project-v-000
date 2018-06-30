482039b4a6ed42915f4294ae22fd8c8b1df836a4
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-30 13:43:39 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -20,14 +20,42 @@ module Players
     end
 
     #1- Find a winning move
+    def winning_move 
+      win_combo = WIN_COMBINATIONS.find do |combo|
+        (@board.cells[combo[0]] == @token && @board.cells[combo[1]] == @token && @board.cells[combo[2]] == " ") || (@board.cells[combo[0]] == @token && @board.cells[combo[2]] == @token && @board.cells[combo[1]] == " ") || (@board.cells[combo[1]] == @token && @board.cells[combo[2]] == @token && @board.cells[combo[0]] == " ")
+      end
+      if win_combo != nil
+        win_cell = win_combo.find do |spot|
+          @board.cells[spot] == " "
+        end
+      end
+    end
+    
 
+    #2- Find a blocking move-- need a way to identify other token first 
+    def other_token
+      if @token == "X"
+        "O"
+      else
+        "X"
+      end
+    end
 
-    #2- Find a blocking move
-
-
-
+    def blocking_move
+      block_combo = WIN_COMBINATIONS.find do |combo|
+        (@board.cells[combo[0]] == other_token && @board.cells[combo[1]] == other_token && @board.cells[combo[2]] == " ") || (@board.cells[combo[0]] == other_token && @board.cells[combo[2]] == other_token && @board.cells[combo[1]] == " ") || (@board.cells[combo[1]] == other_token && @board.cells[combo[2]] == other_token && @board.cells[combo[0]] == " ")
+      end
+      if block_combo != nil
+        block_cell = block_combo.find do |spot|
+          @board.cells[spot] == " "
+        end
+      end
+    end      
+    
     #3- Take the center if it's empty
-
+    def center_empty?
+      @board.cells[4] == " "
+    end
 
     #4- Find the opposite corner
 
